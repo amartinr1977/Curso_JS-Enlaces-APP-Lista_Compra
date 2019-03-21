73bf1c83dbfc77d9b69f635743ac92fe388c9ef4
amartinr1977 <amartinromero@gmail.com>
2019-03-21 20:15:01 +0100

    añadimos la función de borrado de elementos

--- js/main.js
+++ js/main.js
@@ -49,7 +49,8 @@ const MostrarLista = () => {
       <span class="badge badge-primary"> ${listado[i].cantidad}</span>
       <span style="padding: 10px;  background-color: orange; border-radius: 10%">
       ${listado[i].prioridad}</span>
-      <i class="material-icons align-middle float-right" style="font-size: 1.5em; cursor: pointer">delete</i>
+      <span id="identificador" style="display: none">${listado[i].id}</span>
+      <i class="material-icons align-middle float-right" style="font-size: 1.5em; cursor: pointer">delete </i>
       </div>`;
     }
   }
@@ -62,6 +63,26 @@ const AgregarArticulo = e => {
   MostrarLista();
   formulario.reset();
   ComprobarInput();
+};
+
+const EliminarElemento = identificador => {
+  let posicion;
+  for (let j = 0; j < listado.length; j++) {
+    if (listado[j].id === identificador) {
+      posicion = j;
+    }
+  }
+  listado.splice(j, 1);
+  localStorage.setItem("listado", JSON.stringify(listado));
+  MostrarLista();
+};
+
+const Actuar = e => {
+  // console.log(e);
+  if (e.target.innerHTML.trim() === "delete") {
+    console.log(e.path[1].children[4].innerHTML);
+    EliminarElemento(e.path[1].children[4].innerHTML);
+  }
 };
 
 const Incializar = () => {
@@ -79,3 +100,4 @@ const Incializar = () => {
 document.addEventListener("DOMContentLoaded", Incializar);
 articulo.addEventListener("keyup", ComprobarInput);
 agregar.addEventListener("click", AgregarArticulo);
+listacompra.addEventListener("click", Actuar);
